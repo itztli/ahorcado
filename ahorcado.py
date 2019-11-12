d9beb8a0a6cb5b0d0618e579ab62b080d091bb6d
Victor De la Luz <itztli@gmail.com>
2019-11-12 17:33:13 -0600

    reading word

--- ahorcado.py
+++ ahorcado.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 
+import random
+
 with open("words.txt","r") as f:
     words = f.readlines()
 
-print(words[0])
+maximum = len(words)
+n = int(random.random()*maximum)
+
+word = words[n].rstrip()
+
+spaces = len(word)
+
+answer = "X" * spaces
+
+print(word+answer)
